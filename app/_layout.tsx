b9493c526936c911148d019038aedae07af8e19f
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2025-02-19 21:16:42 +0100

    fix(layout): improve main layout structure

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,13 @@
+import { Provider } from 'react-redux';
 import '../global.css';
 
 import { Stack } from 'expo-router';
+import store from './redux/store/store';
 
 export default function Layout() {
-  return <Stack />;
+  return (
+    <Provider store={store}>
+      <Stack />
+    </Provider>
+  );
 }
