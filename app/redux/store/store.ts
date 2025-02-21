52c60c38150b1ca5f6c3b87111d81c5318c05d96
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2025-02-21 10:54:12 +0100

    chore(redux): update store configuration

--- app/redux/store/store.ts
+++ app/redux/store/store.ts
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authSlice from '../Slice/authSlice'
+import categorySlice from '../Slice/categorySlice'
 
 
 const store = configureStore({
   reducer: {
-    auth: authSlice
+    auth: authSlice,
+    category: categorySlice
   },
 });
 
