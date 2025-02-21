4ea815770b1b9b304b04db7135aee1c070f9e389
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2025-02-21 16:41:28 +0100

    fix(api): optimize pet service API integration

--- app/redux/service/api/pets.ts
+++ app/redux/service/api/pets.ts
@@ -2,13 +2,15 @@ import path from "~/app/axios/path"
 
 
 const fetchAllPets = async () => {
-  
     const res = await path.get('pets/findAll')
-    return res.data.pets
-
- 
+    return res.data.pets 
+}
+const fetchPetById = async (petId: string) => {
+    const res = await path.get(`pets/findByPetsId/${petId}`)    
+    return res.data
 }
 
 export {
-    fetchAllPets
+    fetchAllPets,
+    fetchPetById
 }
