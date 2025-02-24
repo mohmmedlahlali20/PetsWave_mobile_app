24104db35dc55314bef4dce55b6f2b95ee042f2d
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2025-02-24 14:28:48 +0000

    refactor(index): optimize app initialization and structure

--- app/index.tsx
+++ app/index.tsx
@@ -1,37 +1,29 @@
-
-import { Stack, useRouter } from "expo-router"
-import { Text, View, Image, ScrollView, TouchableOpacity, Animated } from "react-native"
-import { Feather, Ionicons } from "@expo/vector-icons"
-import { useEffect, useRef } from "react"
-import React from "react"
-import { useAppDispatch, useAppSelector } from "~/hooks/useAppDispatch"
-import { getCategory } from "./redux/Slice/categorySlice"
-import { getPets } from "./redux/Slice/petSlice"
-import { replaceIp } from "~/hooks/helpers"
-import { logout } from "./redux/Slice/authSlice"
-
+import { Stack, useRouter } from 'expo-router';
+import { Text, View, Image, ScrollView, TouchableOpacity, Animated } from 'react-native';
+import { Feather, Ionicons } from '@expo/vector-icons';
+import { useEffect, useRef } from 'react';
+import React from 'react';
+import { useAppDispatch, useAppSelector } from '~/hooks/useAppDispatch';
+import { getCategory } from './redux/Slice/categorySlice';
+import { getPets } from './redux/Slice/petSlice';
+import { replaceIp } from '~/hooks/helpers';
+import { logout } from './redux/Slice/authSlice';
 
 export default function Home() {
-  const dispatch = useAppDispatch()
-  const { categories } = useAppSelector((state) => state.category)
-  const { pets = [], isLoading, error } = useAppSelector((state) => state.pets)
-  const { isAuthenticated } = useAppSelector((state) => state.auth)
-  console.log('====================================');
-  console.log(isAuthenticated);
-  console.log('====================================');
-  const router = useRouter()
-  const scrollY = useRef(new Animated.Value(0)).current
-
+  const dispatch = useAppDispatch();
+  const { categories } = useAppSelector((state) => state.category);
+  const { pets = [], isLoading, error } = useAppSelector((state) => state.pets);
+  const { isAuthenticated } = useAppSelector((state) => state.auth);
+  const router = useRouter();
+  const scrollY = useRef(new Animated.Value(0)).current;
 
   useEffect(() => {
     if (!isAuthenticated) {
       setTimeout(() => {
         router.push('/(auth)/login');
-      }, 100); 
+      }, 100);
     }
   }, [isAuthenticated, router]);
-  
-
 
   const handleLogout = () => {
     dispatch(logout());
@@ -39,62 +31,64 @@ export default function Home() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        await dispatch(getCategory()).unwrap()
-        await dispatch(getPets()).unwrap()
+        await dispatch(getCategory()).unwrap();
+        await dispatch(getPets()).unwrap();
       } catch (err) {
-        console.error("Erreur lors du chargement des données :", err)
+        console.error('Erreur lors du chargement des données :', err);
       }
-    }
+    };
 
-    fetchData()
-  }, [dispatch])
-
+    fetchData();
+  }, [dispatch]);
 
   return (
-    <ScrollView
-      className="flex-1 bg-purple-50"
-      scrollEventThrottle={16}
-    >
+    <ScrollView className="flex-1 bg-purple-50" scrollEventThrottle={16}>
       <Stack.Screen
         options={{
-          title: "PetsWave Shop",
-          headerStyle: { backgroundColor: "#491975" },
-          headerTintColor: "#fff",
-          headerTitleStyle: { fontWeight: "bold" },
+          title: 'PetsWave Shop',
+          headerStyle: { backgroundColor: '#491975' },
+          headerTintColor: '#fff',
+          headerTitleStyle: { fontWeight: 'bold' },
         }}
       />
 
-      <View className="h-48 bg-[#491975] px-4 justify-center">
-        <View className="bg-white/10 p-6 rounded-2xl">
-          <Text className="text-white text-3xl font-bold mb-2">Bienvenue chez PetsWave</Text>
-          <Text className="text-purple-100">Découvrez notre sélection premium pour vos compagnons</Text>
+      <View className="h-48 justify-center bg-[#491975] px-4">
+        <View className="rounded-2xl bg-white/10 p-6">
+          <Text className="mb-2 text-3xl font-bold text-white">Bienvenue chez PetsWave</Text>
+          <Text className="text-purple-100">
+            Découvrez notre sélection premium pour vos compagnons
+          </Text>
         </View>
       </View>
-      <View className="px-4 -mt-6">
-        <View className="bg-white rounded-xl p-4 shadow-lg mb-6">
-          <View className="flex-row justify-around items-center">
-            <TouchableOpacity className="items-center" onPress={() => router.push('/(auth)/profile')}>
-              <View className="bg-purple-600 p-3 rounded-full mb-1 shadow-sm">
+      <View className="-mt-6 px-4">
+        <View className="mb-6 rounded-xl bg-white p-4 shadow-lg">
+          <View className="flex-row items-center justify-around">
+            <TouchableOpacity
+              className="items-center"
+              onPress={() => router.push('/(auth)/profile')}>
+              <View className="mb-1 rounded-full bg-purple-600 p-3 shadow-sm">
                 <Feather name="user" size={20} color="#fff" />
               </View>
               <Text className="text-sm font-medium text-gray-700">Profil</Text>
             </TouchableOpacity>
 
             <TouchableOpacity className="items-center" onPress={() => router.push('/cart/cart')}>
-              <View className="bg-purple-600 p-3 rounded-full mb-1 shadow-sm">
+              <View className="mb-1 rounded-full bg-purple-600 p-3 shadow-sm">
                 <Feather name="shopping-cart" size={20} color="#fff" />
               </View>
               <Text className="text-sm font-medium text-gray-700">Panier</Text>
             </TouchableOpacity>
 
             <TouchableOpacity className="items-center" onPress={handleLogout}>
-              <View className="bg-purple-600 p-3 rounded-full mb-1 shadow-sm">
+              <View className="mb-1 rounded-full bg-purple-600 p-3 shadow-sm">
                 <Feather name="log-out" size={20} color="#fff" />
               </View>
               <Text className="text-sm font-medium text-gray-700">Déconnexion</Text>
             </TouchableOpacity>
-            <TouchableOpacity className="items-center" onPress={() => router.push('/(pets)/serarch')}>
-              <View className="bg-purple-600 p-3 rounded-full mb-1 shadow-sm">
+            <TouchableOpacity
+              className="items-center"
+              onPress={() => router.push('/(pets)/serarch')}>
+              <View className="mb-1 rounded-full bg-purple-600 p-3 shadow-sm">
                 <Feather name="search" size={20} color="#fff" />
               </View>
               <Text className="text-sm font-medium text-gray-700">Search</Text>
@@ -102,26 +96,25 @@ export default function Home() {
           </View>
         </View>
 
-        <Text className="text-xl font-semibold text-gray-800 mb-4">Catégories</Text>
+        <Text className="mb-4 text-xl font-semibold text-gray-800">Catégories</Text>
         <ScrollView horizontal showsHorizontalScrollIndicator={false} className="mb-8">
           {categories?.length > 0 ? (
             categories.map((category, index) => (
               <TouchableOpacity
                 key={Math.random()}
-                className="items-center mr-6"
+                className="mr-6 items-center"
                 style={{
                   transform: [
                     {
                       scale: scrollY.interpolate({
                         inputRange: [0, 100],
                         outputRange: [1, 0.95],
-                        extrapolate: "clamp",
+                        extrapolate: 'clamp',
                       }),
                     },
                   ],
-                }}
-              >
-                <View className="bg-white rounded-2xl p-4 mb-2 shadow-lg border border-purple-100">
+                }}>
+                <View className="mb-2 rounded-2xl border border-purple-100 bg-white p-4 shadow-lg">
                   <Ionicons name="paw" size={32} color="#491975" />
                 </View>
                 <Text className="text-sm font-medium text-gray-700">{category.name}</Text>
@@ -130,13 +123,12 @@ export default function Home() {
           ) : (
             <Text className="text-gray-500">Aucune catégorie disponible</Text>
           )}
-
         </ScrollView>
 
-        <View className="flex-row justify-between items-center mb-6">
+        <View className="mb-6 flex-row items-center justify-between">
           <Text className="text-xl font-semibold text-gray-800">Produits en vedette</Text>
-          <TouchableOpacity className="bg-purple-100 px-4 py-2 rounded-full">
-            <Text className="text-purple-600 font-medium">Voir tout</Text>
+          <TouchableOpacity className="rounded-full bg-purple-100 px-4 py-2">
+            <Text className="font-medium text-purple-600">Voir tout</Text>
           </TouchableOpacity>
         </View>
 
@@ -144,46 +136,41 @@ export default function Home() {
           <ScrollView
             horizontal={false}
             showsVerticalScrollIndicator={false}
-            contentContainerStyle={{ gap: 16, padding: 16 }}
-          >
+            contentContainerStyle={{ gap: 16, padding: 16 }}>
             {pets.map((pet, index) => (
               <TouchableOpacity
                 key={index}
-                className="bg-white rounded-2xl overflow-hidden shadow-lg"
+                className="overflow-hidden rounded-2xl bg-white shadow-lg"
                 activeOpacity={0.8}
-                onPress={() => router.push(`/petsDetails?petId=${pet._id}`)}
-              >
+                onPress={() => router.push(`/petsDetails?petId=${pet._id}`)}>
                 {pet.images && pet.images.length > 0 ? (
                   <Image
                     source={{ uri: replaceIp(pet.images[0], '192.168.8.134') }}
-                    className="w-full h-56"
+                    className="h-56 w-full"
                     resizeMode="cover"
                   />
                 ) : (
-                  <View className="w-full h-56 bg-gray-200 flex justify-center items-center">
+                  <View className="flex h-56 w-full items-center justify-center bg-gray-200">
                     <Text className="text-gray-500">Image non disponible</Text>
                   </View>
                 )}
 
-                <View className="p-4 space-y-2">
-                  <View className="flex-row justify-between items-start">
+                <View className="space-y-2 p-4">
+                  <View className="flex-row items-start justify-between">
                     <View>
                       <Text className="text-lg font-semibold text-gray-800">{pet.name}</Text>
                       <Text className="text-sm text-gray-500">
-                        {pet.description.split(" ").slice(0, 4).join(" ")}...
+                        {pet.description.split(' ').slice(0, 4).join(' ')}...
                       </Text>
-
                     </View>
-                    <View className="bg-purple-100 px-3 py-1 rounded-full">
-                      <Text className="text-purple-600 font-bold">{pet.Prix} MAD</Text>
+                    <View className="rounded-full bg-purple-100 px-3 py-1">
+                      <Text className="font-bold text-purple-600">{pet.Prix} MAD</Text>
                     </View>
                   </View>
 
-                  <TouchableOpacity
-                    className="bg-gradient-to-r from-purple-600 to-purple-400 py-2 rounded-xl flex-row justify-center items-center"
-                  >
+                  <TouchableOpacity className="flex-row items-center justify-center rounded-xl bg-gradient-to-r from-purple-600 to-purple-400 py-2">
                     <Feather name="shopping-cart" size={18} color="#fff" className="mr-2" />
-                    <Text className="text-white font-semibold">Ajouter au panier</Text>
+                    <Text className="font-semibold text-white">Ajouter au panier</Text>
                   </TouchableOpacity>
                 </View>
               </TouchableOpacity>
@@ -191,12 +178,10 @@ export default function Home() {
           </ScrollView>
         ) : (
           <View className="flex-1 items-center justify-center">
-            <Text className="text-gray-500 text-lg">Aucun animal disponible</Text>
+            <Text className="text-lg text-gray-500">Aucun animal disponible</Text>
           </View>
         )}
-
       </View>
     </ScrollView>
-  )
+  );
 }
-
