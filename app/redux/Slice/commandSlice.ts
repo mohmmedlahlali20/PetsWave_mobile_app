6ae1bd3fa074221ab4dbe192622f2b93f94deace
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2025-02-24 10:00:11 +0000

    feat(command): implement command slice for order processing

--- app/redux/Slice/commandSlice.ts
+++ app/redux/Slice/commandSlice.ts
@@ -1,22 +1,55 @@
-import { createSlice } from "@reduxjs/toolkit"
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
 import { Commands, Status } from "~/constant/type"
+import { commandApi } from "../service/api/command"
+import AsyncStorage from "@react-native-async-storage/async-storage"
 
 
 
 
-const initialState:{
+const initialState: {
     isLoading: boolean,
     status: Status,
     error: string | null,
     commands: Commands[],
     selectedCommands: Commands | null
-}={
+} = {
     isLoading: false,
     status: Status.Pending,
     error: null,
     commands: [],
     selectedCommands: null
 }
+
+
+
+export const command = createAsyncThunk('passeCommand/command',
+    async (
+        {
+            petsId,
+            userId,
+            totalamount }:
+            {
+                petsId: string[];
+                userId:
+                string,
+                totalamount: number
+            },
+        {
+            rejectWithValue
+        }
+    ) => {
+        try {
+            const userID = await AsyncStorage.getItem('token')
+            return await commandApi({
+                petsId,
+                userId,
+                totalamount
+            })
+        } catch (err: any) {
+            return rejectWithValue(err.response?.data?.message || 'cannot get Category failed');
+
+        }
+    })
 
 
 
@@ -27,7 +60,16 @@ const commandSlice = createSlice({
     reducers: {},
     extraReducers: (builder) => {
         builder
-    }
+        .addCase(command.pending, (state)=>{
+            state.isLoading = true,
+            state.error = null
+        })
+        .addCase(command.fulfilled, (state, action)=>{
+            state.isLoading = false,
+            state.error=null
+        })
+     
+    },
 })
 
 
