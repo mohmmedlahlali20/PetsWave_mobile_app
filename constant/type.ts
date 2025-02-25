734445837d14d31ef03816f34d770b407499a8c5
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2025-02-25 19:49:07 +0000

    chore(types): update type definitions

--- constant/type.ts
+++ constant/type.ts
@@ -9,7 +9,6 @@ export interface User {
 
 export interface Category {
     _id: any
-
     name: string
 }
 
@@ -45,8 +44,8 @@ export interface Pets {
 
 export interface Commands {
     _id: string
-    petsId: string,
-    userId: string,
+    petsId: Pets,
+    userId: User,
     status: Status,
     orderDate: Date,
     totalAmount: number
@@ -55,7 +54,7 @@ export interface Commands {
 
 export enum Status {
     Pending = "pending",
-    InProgress = "inProgress",
+    InProgress = "InProgress",
     Completed = "completed",
     Cancelled = "cancelled"
 }
