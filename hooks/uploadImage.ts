7040e1217bc8aac2ddb72f5a3aca0b376f9c0fb8
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2025-02-26 17:05:59 +0000

    feat(hooks): add image upload utility

---
+++ hooks/uploadImage.ts
@@ -0,0 +1,36 @@
+import axios from 'axios';
+
+
+export const uploadImageToBackend = async (imageUri: string ): Promise<string | null> => {
+  const response = await fetch(imageUri);
+  console.log('====================================');
+  console.log(response);
+  console.log('====================================');
+  const blob = await response.blob();
+  const file = new File([blob], 'product-image.jpg', { type: blob.type });
+console.log(file)
+  const formData = new FormData();
+formData.append('avatar', {
+    uri: imageUri,
+    type: 'image/jpeg',
+    name: 'image.jpg',
+    });
+
+const config = {
+  headers: {
+    'Content-Type': 'multipart/form-data',
+  },
+};
+
+try {
+  const response = await axios.post(`http://${process.env.EXPO_PUBLIC_URL}:${process.env.EXPO_PUBLIC_PORT}/upload`, formData, config);
+  console.log("fuck",response.data.imageUrl);
+  
+  return response.data.imageUrl
+} catch (error) {
+  console.error('Error uploading image:', error);
+  return 'https://i.pinimg.com/736x/70/8c/08/708c08614099f90b849c6f7089f8effb.jpg'
+
+}
+
+};
