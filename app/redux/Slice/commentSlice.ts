a3c5244b572b134cffbba5aafc978c25b0573e5b
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2025-03-05 23:43:25 +0000

    fix(redux): update comment slice logic

--- app/redux/Slice/commentSlice.ts
+++ app/redux/Slice/commentSlice.ts
@@ -1,54 +1,52 @@
-import { createAsyncThunk, createSlice,PayloadAction } from "@reduxjs/toolkit";
-import { Comments } from "~/constant/type";
-import { getComments } from "../service/api/comment";
+import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit"
+import { Comments } from "~/constant/type"
+import { getCommentsByPetsId } from "../service/api/comment"
 
-
-
-const initialState: {
-    isLoading: boolean;
-    error: string | null
-    comments: Comments[]
-
-} = {
-    isLoading: false,
-    error: null,
-    comments: []
+interface CommentState {
+  isLoading: boolean
+  error: string | null
+  comments: Comments[]
 }
 
+const initialState: CommentState = {
+  isLoading: false,
+  error: null,
+  comments: [],
+}
 
-export const GetAllComments = createAsyncThunk('getCommenst/comments',
-    async (_, { rejectWithValue }) => {
-        try {
-            return await getComments()
-        } catch (err: any) {
-            return rejectWithValue(err.response?.data?.message || "cannot get Comments failed");
-        }
-
-    })
-
+export const GetAllComments = createAsyncThunk(
+  "comments/getAll",
+  async ({ petsId }: { petsId: string }, { rejectWithValue }) => {
+    try {
+      return await getCommentsByPetsId(petsId)
+    } catch (err: any) {
+      return rejectWithValue(err.response?.data?.message || "Échec du chargement des commentaires")
+    }
+  }
+)
 
 const commentSlice = createSlice({
-    name: "comments",
-    initialState,
-    reducers: {},
-    extraReducers: async (builder) => {
-        builder
-        .addCase(GetAllComments.pending, (state)=>{
-            state.isLoading= true
-            state.error= null
-        })
-        .addCase(GetAllComments.fulfilled, (state, action:PayloadAction<Comments[]>)=>{
-            state.comments = action.payload
-            state.isLoading = false;
-            state.error = null
-        })
-        .addCase(GetAllComments.rejected, (state, action)=>{
-            state.isLoading = true
-            state.error = action.payload as string
-        })
-    }
+  name: "comments",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(GetAllComments.pending, (state) => {
+        state.isLoading = true
+        state.error = null
+      })
+      .addCase(GetAllComments.fulfilled, (state, action: PayloadAction<Comments[]>) => {
+        console.log(action.payload);
+        
+        state.comments = action.payload
+        state.isLoading = false
+        state.error = null
+      })
+      .addCase(GetAllComments.rejected, (state, action) => {
+        state.isLoading = false
+        state.error = action.payload as string
+      })
+  },
 })
-
-
 
 export default commentSlice.reducer
